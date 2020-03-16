acc66ff5e597745f0d9fae5a194127460a1323f3
way2nepdroid <ansarianash.np@gmail.com>
2020-03-16 11:23:17 +0530

    Initail commit

---
+++ src/main/java/com/nepdroid/HelloResource.java
@@ -0,0 +1,52 @@
+package com.nepdroid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nepdroid.models.AuthenticationRequest;
+import com.nepdroid.models.AuthenticationResponse;
+import com.nepdroid.services.MyUserDetailsService;
+import com.nepdroid.util.JwtUtil;
+
+@RestController
+public class HelloResource {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+	
+	@Autowired
+	private JwtUtil jwtTokenUtil;
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World";
+	}
+	
+	@PostMapping("/authenticate")
+	public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequest authenticationRequest) throws Exception{
+		try {
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),authenticationRequest.getPassword()));
+		}
+		catch(BadCredentialsException e) {
+			throw new Exception("Incorrect username or password");
+		}
+		
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+		final String jwt = jwtTokenUtil.generateToken(userDetails);
+		
+		return ResponseEntity.ok(new AuthenticationResponse(jwt)); 
+	}
+	
+}
---
+++ src/test/java/com/nepdroid/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nepdroid;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
